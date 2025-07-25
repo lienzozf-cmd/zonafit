6d5b7171db9b31a4a4bb64692c375c27811f780b
Francis R <rabanalesf22@gmail.com>
2025-07-25 02:22:47 +0000

    QUIERO QUE ASI SE VEA ESTE APARTADO DE HOMBRE, MUJER, ACCESORIOS, SUPLEM

--- src/components/category-grid.tsx
+++ src/components/category-grid.tsx
@@ -8,35 +8,35 @@ const CategoryGrid = () => {
         <p>"Desata tu mejor versión con fuerza, estilo y elegancia. Adentrate a ZONA FIT GT y se tu mejor versión: donde tu energía se viste, se nutre y se luce.".</p>
       </div>
       <div className="image-grid">
-        <div className="grid-item large-vertical">
+        <div className="grid-item mujer">
           <Image src="/assets/images/marcas/gymshark/mujer/anabelrojo2.jpg" alt="Mujer" width={500} height={1000} data-ai-hint="woman fitness" className="object-cover w-full h-full" />
           <div className="overlay-text">
             <h3>Mujer</h3>
             <p>47 productos</p>
           </div>
         </div>
-        <div className="grid-item large-horizontal">
+        <div className="grid-item hombre">
           <Image src="/assets/images/marcas/youngla/hombre/jerdaniv.jpg" alt="Hombre" width={1000} height={500} data-ai-hint="man fitness" className="object-cover w-full h-full" />
           <div className="overlay-text">
             <h3>Hombre</h3>
             <p>24 productos</p>
           </div>
         </div>
-        <div className="grid-item small-square">
+        <div className="grid-item accesorios">
           <Image src="/assets/images/marcas/youngla/hombre/maletagym.png" alt="Accesorios" width={500} height={500} data-ai-hint="gym accessories" className="object-cover w-full h-full" />
           <div className="overlay-text">
             <h3>Accesorios</h3>
             <p>24 productos</p>
           </div>
         </div>
-        <div className="grid-item small-square">
+        <div className="grid-item suplementos">
           <Image src="/assets/images/marcas/raw/prewcb.jpg" alt="Suplementos" width={500} height={500} data-ai-hint="supplements" className="object-cover w-full h-full" />
           <div className="overlay-text">
             <h3>Suplementos</h3>
             <p>24 productos</p>
           </div>
         </div>
-        <div className="grid-item small-square">
+        <div className="grid-item joyeria">
           <Image src="/assets/images/marcas/rgmnt/tridente.png" alt="Joyeria" width={500} height={500} data-ai-hint="jewelry" className="object-cover w-full h-full" />
           <div className="overlay-text">
             <h3>Joyeria</h3>
