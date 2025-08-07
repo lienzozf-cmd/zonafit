a455c1bf937c0bcac93fb42523334975ae5f0077
Francis R <rabanalesf22@gmail.com>
2025-08-07 01:23:16 +0000

    quiero que el carrito se vea rojo el icono y esten un poco mas a la izqu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -229,7 +229,7 @@ const Header = () => {
         <div className="header-icons">
          {isMobile === false && renderSearch(false)}
           <div className="relative">
-            <ShoppingCart className="cart-icon" onClick={() => setIsCartOpen(true)}/>
+            <ShoppingCart className="cart-icon" color="hsl(var(--accent))" onClick={() => setIsCartOpen(true)}/>
             {itemCount > 0 && (
                <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
                 {itemCount}
