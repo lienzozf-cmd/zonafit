f417a06d1ea36e739fec2462e048406fc8763427
Francis R <rabanalesf22@gmail.com>
2025-08-31 02:29:32 +0000

    agrega el banner7

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -18,6 +18,7 @@ const banners = [
   { src: '/assets/images/banners/banner4.png', alt: 'Banner 4' },
   { src: '/assets/images/banners/banner5.png', alt: 'Banner 5' },
   { src: '/assets/images/banners/banner6.png?v=2', alt: 'Banner 6' },
+  { src: '/assets/images/banners/banner7.png', alt: 'Banner 7' },
 ];
 
 const HeroSection = () => {
