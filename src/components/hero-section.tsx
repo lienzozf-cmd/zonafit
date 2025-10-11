afead18d69681350e7f3dd2cf7b768d3f078ee3b
Francis R <rabanalesf22@gmail.com>
2025-10-11 19:54:06 +0000

    QUITA LOS BANNERS 6 Y 7

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -17,8 +17,6 @@ const banners = [
   { src: '/assets/images/banners/banner3.png', alt: 'Banner 3' },
   { src: '/assets/images/banners/banner4.png', alt: 'Banner 4' },
   { src: '/assets/images/banners/banner5.png', alt: 'Banner 5' },
-  { src: '/assets/images/banners/banner6.png?v=2', alt: 'Banner 6' },
-  { src: '/assets/images/banners/banner7.png', alt: 'Banner 7' },
 ];
 
 const HeroSection = () => {
