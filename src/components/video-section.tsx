6a1479603b0df4402b1555d9b6f1f00f1343c23e
Francis R <rabanalesf22@gmail.com>
2025-10-28 03:05:21 +0000

    necesito que en el modo de ventana de navegador a la mitad se ve asi de

--- src/components/video-section.tsx
+++ src/components/video-section.tsx
@@ -3,7 +3,7 @@ const VideoSection = () => {
         <div className="video-container">
             <div className="video-overlay"></div>
             <video width="2000" height="750" autoPlay loop muted playsInline>
-                <source src="/assets/videos/cbumtraining.mp4" type="video/mp4" />
+                <source src="/assets/videos/Onyx.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
         </div>
