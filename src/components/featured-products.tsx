6eb7587411811e6b0770fc8f999085a3459f968c
Francis R <rabanalesf22@gmail.com>
2025-11-21 19:44:22 +0000

    reviertelo

--- src/components/featured-products.tsx
+++ src/components/featured-products.tsx
@@ -7,7 +7,7 @@ const FeaturedProducts = () => {
   // Define a specific list of featured product IDs
   const featuredProductIds = [1, 2, 3, 4, 5, 2643, 6, 7, 8];
   
-  const featuredProducts = (products || []).filter(product =>
+  const featuredProducts = products.filter(product =>
     featuredProductIds.includes(product.id)
   ).sort((a, b) => featuredProductIds.indexOf(a.id) - featuredProductIds.indexOf(b.id));
 
