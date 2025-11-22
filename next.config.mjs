f7cc8d5abb679b6a4ed1987c14c459ffcd23b6fc
Francis R <rabanalesf22@gmail.com>
2025-11-22 15:13:47 +0000

    ahora no toques nada que tenga que ver con correo quiero que en general

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,43 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  child-src 'self';
+  style-src 'self' 'unsafe-inline';
+  font-src 'self';  
+`;
+
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload',
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+                    }
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
