530cd7b0ac7484cc25bd9422e472abbd525c71d0
Francis R <rabanalesf22@gmail.com>
2025-11-24 04:00:58 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/emails/order-confirmation-email.tsx
+++ src/components/emails/order-confirmation-email.tsx
@@ -88,7 +88,7 @@ import {
                       <td style={tableCell}>
                         <Row>
                           <Column style={{ paddingRight: '15px' }}>
-                             <Img src={`${baseUrl}${item.image}`} alt={item.name} width="60" height="60" style={productImage} />
+                             <Img src={item.image.startsWith('http') ? item.image : `${baseUrl}${item.image}`} alt={item.name} width="60" height="60" style={productImage} />
                           </Column>
                           <Column>
                             <Text style={{ ...text, margin: 0, fontWeight: 'bold' }}>{item.name}</Text>
