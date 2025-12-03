351c1dffe4e27ef9c88c96d453d44b244d677c59
Francis R <rabanalesf22@gmail.com>
2025-12-03 00:21:02 +0000

    YA FUNCIONA EL ENVIO DEL CORREO PERO ACABO DE NOTAR QUE AGREGASTE INFORM

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -134,7 +134,7 @@ const ProductCard = ({ product: initialProduct }: ProductCardProps) => {
       return;
     }
     
-    const priceAsNumber = parseFloat(product.price.replace(/[^0-9.]/g, ''));
+    const priceAsNumber = parseFloat(product.price.replace(/[^\d.]/g, ''));
     const cartItemId = selectedColor ? `${product.id}-${selectedColor.name}-${selectedOption.value}` : `${product.id}-default-${selectedOption.value}`;
     const cartItemName = selectedColor ? `${product.name} - ${selectedColor.name}` : product.name;
 
@@ -253,4 +253,3 @@ const ProductCard = ({ product: initialProduct }: ProductCardProps) => {
 };
 
 export default ProductCard;
-
