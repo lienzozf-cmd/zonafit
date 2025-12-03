95c203fd7ab6d1ed669324bbe6c17f164d39a816
Francis R <rabanalesf22@gmail.com>
2025-12-03 00:31:42 +0000

    VUELVE A ACTIVAR LA INTRO QUE TENIA LA PAGINA PORQUE YA NO SE MUESTRA LA

--- src/components/intro-wrapper.tsx
+++ src/components/intro-wrapper.tsx
@@ -1,46 +1,46 @@
 
 'use client';
 
-// import { useState, useEffect } from 'react';
-// import IntroAnimation from './intro-animation';
+import { useState, useEffect } from 'react';
+import IntroAnimation from './intro-animation';
 
 export default function IntroWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const [introFinished, setIntroFinished] = useState(true);
-  // const [isClient, setIsClient] = useState(false);
-  //
-  // useEffect(() => {
-  //   setIsClient(true);
-  //   try {
-  //     if (sessionStorage.getItem('introShown')) {
-  //       setIntroFinished(true);
-  //     } else {
-  //       setIntroFinished(false);
-  //     }
-  //   } catch (error) {
-  //     setIntroFinished(true);
-  //   }
-  // }, []);
-  //
-  // const handleIntroFinish = () => {
-  //   setIntroFinished(true);
-  //   try {
-  //     sessionStorage.setItem('introShown', 'true');
-  //   } catch (error) {
-  //     // Handle potential errors with sessionStorage
-  //   }
-  // };
-  //
-  // if (!isClient) {
-  //   return null;
-  // }
-  //
-  // if (!introFinished) {
-  //   return <IntroAnimation onIntroFinish={handleIntroFinish} />;
-  // }
+  const [introFinished, setIntroFinished] = useState(true);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+    try {
+      if (sessionStorage.getItem('introShown')) {
+        setIntroFinished(true);
+      } else {
+        setIntroFinished(false);
+      }
+    } catch (error) {
+      setIntroFinished(true);
+    }
+  }, []);
+
+  const handleIntroFinish = () => {
+    setIntroFinished(true);
+    try {
+      sessionStorage.setItem('introShown', 'true');
+    } catch (error) {
+      // Handle potential errors with sessionStorage
+    }
+  };
+
+  if (!isClient) {
+    return null;
+  }
+
+  if (!introFinished) {
+    return <IntroAnimation onIntroFinish={handleIntroFinish} />;
+  }
 
   return <>{children}</>;
 }
