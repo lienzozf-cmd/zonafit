53656d63345403c486a970403ea992cf6f86e328
Francis R <rabanalesf22@gmail.com>
2025-12-10 03:19:24 +0000

    REINICIA EL CONTADOR A 000000

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3346,7 +3346,7 @@ export const products: Product[] = [
           "values": [
             {
               "value": "S",
-              "stock": 1
+              "stock": 0
             },
             {
               "value": "M",
