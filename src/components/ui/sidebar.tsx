7fa25c9aab40d9c6965f52607914934e31b7fb1a
Francis R <rabanalesf22@gmail.com>
2025-12-14 18:59:16 +0000

    Hola. Veo que has corregido el error anterior de dotenv, ¡lo cual es gen

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -67,7 +67,8 @@ const SidebarProvider = React.forwardRef<
     },
     ref
   ) => {
-    const isMobile = useIsMobile()
+    const isMobileValue = useIsMobile()
+    const isMobile = isMobileValue ?? false
     const [openMobile, setOpenMobile] = React.useState(false)
 
     // This is the internal state of the sidebar.
