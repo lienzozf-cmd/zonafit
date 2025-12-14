63a0a1f316cf1a9d1f566ca5baafab9e69329f81
Francis R <rabanalesf22@gmail.com>
2025-12-14 04:05:05 +0000

    SIGUE SIN AGREGARLE LOS Q.35 de envio deberia de estar visible y la suma

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3389,7 +3389,7 @@ export const products: Product[] = [
       "values": [
         {
           "value": "S",
-          "stock": 1
+          "stock": 0
         },
         {
           "value": "M",
