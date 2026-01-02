0b72c0daf66f8490bd5b8eba792bcb94acbd998b
F F <rabanalesf22@gmail.com>
2026-01-02 17:39:46 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,15 @@
 /** @type {import('next').NextConfig} */
-
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline';
-  child-src 'self';
-  style-src 'self' 'unsafe-inline';
-  font-src 'self';  
-`;
-
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff',
-                    },
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'SAMEORIGIN',
-                    },
-                    {
-                        key: 'Strict-Transport-Security',
-                        value: 'max-age=63072000; includeSubDomains; preload',
-                    },
-                    {
-                        key: 'X-XSS-Protection',
-                        value: '1; mode=block',
-                    },
-                    {
-                        key: 'Content-Security-Policy',
-                        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
-                    }
-                ],
-            },
-        ];
-    },
+  devIndicators: {
+    buildActivity: false,
+  },
+  experimental: {
+    // This is needed to allow the Next.js dev server to be proxied in the current environment.
+    // https://nextjs.org/docs/app/api-reference/config/next-config-js/allowedDevOrigins
+    allowedDevOrigins: [
+      'https://9000-firebase-zona-fit-gt1-1765734394617.cluster-pgviq6mvsncnqxx6kr7pbz65v6.cloudworkstations.dev',
+    ],
+  },
 };
 
 export default nextConfig;
