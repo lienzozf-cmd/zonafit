4a5d2b3b2064bc5d898efad94768d04311f51111
F F <rabanalesf22@gmail.com>
2026-01-25 21:07:38 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      }
-    ]
-  },
-  experimental: {
-    serverComponentsExternalPackages: ["@react-email/components"],
-  },
-  // Adding env configuration
-  env: {
-    SMTP_EMAIL: process.env.SMTP_EMAIL,
-    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY
-  },
+  serverExternalPackages: ['@react-email/components', 'nodemailer'],
 };
 
 export default nextConfig;
